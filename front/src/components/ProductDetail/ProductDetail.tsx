a5b9a89eab6731632f120c4ac8207f5f43a57c43
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2024-12-16 11:59:36 -0300

    Restrict addToCart functionality to logged-in users only

--- front/src/components/ProductDetail/ProductDetail.tsx
+++ front/src/components/ProductDetail/ProductDetail.tsx
@@ -2,13 +2,14 @@
 
 import { IProduct } from "@/Interfaces/interfaces";
 import { getProductById } from "@/services/services";
-import { useCartStore } from "@/store";
+import { useCartStore, useUserStore } from "@/store";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 export function ProductDetail({ id }: { id: string }) {
   const router = useRouter();
+  const { userData } = useUserStore();
   const { addToCart } = useCartStore();
 
   const [product, setProduct] = useState<IProduct>();
@@ -27,7 +28,6 @@ export function ProductDetail({ id }: { id: string }) {
         title: "Producto no encontrado",
         text: "Intenta con otro producto del catálogo.",
         icon: `warning`,
-        showCancelButton: true,
         confirmButtonText: "Volver",
         reverseButtons: true,
         allowOutsideClick: false,
@@ -44,6 +44,37 @@ export function ProductDetail({ id }: { id: string }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
+  const handleAddToCart = () => {
+    if (!userData?.token) {
+      Swal.fire({
+        title: "",
+        text: "Debes iniciar sesión para agregar un producto al carrito",
+        icon: "warning",
+        showCloseButton: true,
+        confirmButtonText: "Iniciar sesión",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router.push("/login");
+        }
+      });
+    } else {
+      if (product) {
+        addToCart(product);
+        Swal.fire({
+          title: "Agregado",
+          text: "El producto fué incluído en tu carrito",
+          icon: `success`,
+          showCloseButton: true,
+          confirmButtonText: "Al carrito",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            router.push("/cart");
+          }
+        });
+      }
+    }
+  };
+
   return (
     <>
       <div className="w-1/4">
@@ -51,15 +82,7 @@ export function ProductDetail({ id }: { id: string }) {
         <img src={product?.image} alt=""></img>
         <h1>{product?.name}</h1>
         <p>${product?.price}</p>
-        <button
-          onClick={() => {
-            if (product) {
-              addToCart(product);
-            }
-          }}
-          className="bg-green-400"
-          disabled={!product}
-        >
+        <button onClick={() => handleAddToCart()} className="bg-green-400">
           Agregar al Carrito
         </button>
       </div>
