6e332bfc6f6117bc8c9ef15ad78ff470d7d9cd74
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-01-15 01:04:49 -0300

    fix| vercel deployment: params bug - changed semantic

--- front/src/app/products/[productID]/page.tsx
+++ front/src/app/products/[productID]/page.tsx
@@ -1,10 +1,26 @@
 import { ProductDetail } from "@/components/ProductDetail/ProductDetail";
 
-export default function SingleProduct({
-  params,
-}: {
-  params: { productID: string };
-}) {
+// export default function SingleProduct({
+//   params,
+// }: {
+//   params: { productID: string };
+// }) {
+//   const { productID } = params;
+
+//   return (
+//     <>
+//       <ProductDetail id={productID} />
+//     </>
+//   );
+// }
+
+interface PageProps {
+  params: {
+    productID: string;
+  };
+}
+
+export default function SingleProduct({ params }: PageProps) {
   const { productID } = params;
 
   return (
