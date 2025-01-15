6cc3a06f498149de8244db3bce28719f5e3a8143
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-01-15 01:22:33 -0300

    fix| vercel deployment: reinstalled dependencies, changed tsconfig.js and disabled eslint for build

--- front/src/app/products/[productID]/page.tsx
+++ front/src/app/products/[productID]/page.tsx
@@ -1,26 +1,10 @@
 import { ProductDetail } from "@/components/ProductDetail/ProductDetail";
 
-// export default function SingleProduct({
-//   params,
-// }: {
-//   params: { productID: string };
-// }) {
-//   const { productID } = params;
-
-//   return (
-//     <>
-//       <ProductDetail id={productID} />
-//     </>
-//   );
-// }
-
-interface PageProps {
-  params: {
-    productID: string;
-  };
-}
-
-export default function SingleProduct({ params }: PageProps) {
+export default function SingleProduct({
+  params,
+}: {
+  params: { productID: string };
+}) {
   const { productID } = params;
 
   return (
