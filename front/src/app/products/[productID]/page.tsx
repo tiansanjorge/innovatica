34898c0b2f81c559177a9c22829ae52e36fda19c
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-01-15 00:44:44 -0300

    fix| vercel deployment: deleted async function

--- front/src/app/products/[productID]/page.tsx
+++ front/src/app/products/[productID]/page.tsx
@@ -1,6 +1,6 @@
 import { ProductDetail } from "@/components/ProductDetail/ProductDetail";
 
-export default async function SingleProduct({
+export default function SingleProduct({
   params,
 }: {
   params: { productID: string };
