514bd2805e9f709ec9e8c6d47eb3b4efd1a09fcf
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-01-20 12:04:08 -0300

    style: products responsive

--- front/src/components/CardProduct/CardProduct.tsx
+++ front/src/components/CardProduct/CardProduct.tsx
@@ -9,11 +9,11 @@ export function CardProduct({ product }: { product: IProduct }) {
   return (
     <Link
       href={`/products/${id.toString()}`}
-      className="hover:scale-105 transition-transform duration-500 ease-in-out"
+      className="hover:scale-105 transition-transform duration-500 ease-in-out "
     >
-      <GlassEffectDiv className="flex flex-col justify-between text-center m-2 rounded-xl h-72">
-        <div className="flex items-center flex-grow px-3">
-          <img src={image} alt="" className="w-64"></img>
+      <GlassEffectDiv className="flex flex-col justify-between text-center m-2 rounded-xl ">
+        <div className="flex items-center p-3 h-72">
+          <img src={image} alt={`${name}`} className="w-64"></img>
         </div>
         <div className="bg-customBlue text-gray-100 rounded-b-xl p-2">
           <h3>{name}</h3>
--- front/src/components/Products/Products.tsx
+++ front/src/components/Products/Products.tsx
@@ -3,7 +3,7 @@ import { CardProduct } from "../CardProduct/CardProduct";
 
 export function Products({ products }: { products: IProduct[] }) {
   return (
-    <div className="flex justify-evenly  flex-wrap xl:flex-nowrap my-5 lg:w-3/4 self-center p-4 bg-customGreen rounded-xl">
+    <div className="flex justify-evenly flex-wrap xl:flex-nowrap m-4 xl:mx-0 xl:my-4 py-3 lg:px-8 xl:px-4 xl:w-11/12 self-center bg-customGreen rounded-xl">
       {products.map((product, index) => (
         <CardProduct key={index} product={product} />
       ))}
