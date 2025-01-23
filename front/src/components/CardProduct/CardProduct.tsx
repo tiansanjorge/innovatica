55531379595fd23a2d6e2e5c14d1c566360cd294
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-01-22 22:47:38 -0300

    style: adjusted wdth in cardProducts images

--- front/src/components/CardProduct/CardProduct.tsx
+++ front/src/components/CardProduct/CardProduct.tsx
@@ -13,7 +13,11 @@ export function CardProduct({ product }: { product: IProduct }) {
     >
       <GlassEffectDiv className="flex flex-col justify-between text-center m-2 rounded-xl ">
         <div className="flex items-center p-3 h-72">
-          <img src={image} alt={`${name}`} className="w-64"></img>
+          <img
+            src={image}
+            alt={`${name}`}
+            className="w-44 sm:w-52 lg:w-64"
+          ></img>
         </div>
         <div className="bg-customBlue text-gray-100 rounded-b-xl p-2">
           <h3>{name}</h3>
